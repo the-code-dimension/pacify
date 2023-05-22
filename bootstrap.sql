93ddbc7617f4bfbde562c92afa7dd1b8c7a4f52d
The Code Dimension <codedimension@protonmail.com>
2023-05-22 17:21:59 -0400

    Converted indentation in install.sql

--- bootstrap.sql
+++ bootstrap.sql
@@ -2,7 +2,7 @@
  *
  */
 CREATE PROCEDURE Pacify.Bootstrap AS BEGIN
-
+-- output an initial header
 DECLARE @hrule NVARCHAR(120) = REPLICATE('-', 120);
 PRINT @hrule;
 PRINT 'Pacify Bootstrap Procedure';
--- install.sql
+++ install.sql
@@ -26,75 +26,75 @@ SET NOCOUNT ON;
 DECLARE @hrule NCHAR(120) = REPLICATE('-', 120);
 PRINT @hrule;
 DROP TABLE IF EXISTS
-	#tblDropQueries;
+    #tblDropQueries;
 CREATE TABLE #tblDropQueries (
-	[Index]		INT,
-	[DropQuery]	NVARCHAR(MAX)
+    [Index]		INT,
+    [DropQuery]	NVARCHAR(MAX)
 );
 WITH cteObjectTypes AS (
-	SELECT
-		'P' AS [Type],
-		'PROCEDURE' AS [Name]
-	UNION
-	SELECT
-		'T',
-		'TABLE'
-	UNION
-	SELECT
-		'V',
-		'VIEW'
+    SELECT
+        'P' AS [Type],
+        'PROCEDURE' AS [Name]
+    UNION
+    SELECT
+        'T',
+        'TABLE'
+    UNION
+    SELECT
+        'V',
+        'VIEW'
 )
 INSERT INTO
-	#tblDropQueries
+    #tblDropQueries
 SELECT
-	ROW_NUMBER() OVER (
-		ORDER BY
-			a.[name]
-	) AS [Index],
-	CONCAT(
-		'DROP ',
-		c.[Name],
-		' Pacify.[', a.[name], '];'
-	) AS DropQuery
+    ROW_NUMBER() OVER (
+        ORDER BY
+            a.[name]
+    ) AS [Index],
+    CONCAT(
+        'DROP ',
+        c.[Name],
+        ' Pacify.[', a.[name], '];'
+    ) AS DropQuery
 FROM
-	sys.objects AS a
+    sys.objects AS a
 LEFT JOIN sys.schemas AS b ON
-	a.[schema_id] = b.[schema_id]
+    a.[schema_id] = b.[schema_id]
 LEFT JOIN cteObjectTypes AS c ON
-	a.[type] = c.[Type]
+    a.[type] = c.[Type]
 WHERE
-	b.[name] = 'Pacify';
+    b.[name] = 'Pacify';
 
 -- loop over all of the derived DROP queries
 DECLARE @index INT = 1;
 WHILE @index <= (SELECT MAX([Index]) FROM #tblDropQueries) BEGIN
-	DECLARE @dropQuery NVARCHAR(MAX) = (
-		SELECT
-			[DropQuery]
-		FROM
-			#tblDropQueries
-		WHERE
-			[Index] = @index
-	);
-	PRINT @dropQuery;
+    DECLARE @dropQuery NVARCHAR(MAX) = (
+        SELECT
+            [DropQuery]
+        FROM
+            #tblDropQueries
+        WHERE
+            [Index] = @index
+    );
+    PRINT @dropQuery;
 
-	-- execute the DROP query
-	EXEC sp_executesql
-		@dropquery;
+    -- execute the DROP query
+    EXEC sp_executesql
+        @dropquery;
 
-	SET @index = @index + 1;
+    SET @index = @index + 1;
 END;
 
 PRINT 'DROP SCHEMA IF EXISTS [Pacify];';
 DROP SCHEMA IF EXISTS
-	[Pacify];
+    [Pacify];
 GO
 */
 
 
 -- create a schema to contain all Pacify resources
 CREATE SCHEMA
-	[Pacify];
+    [Pacify];
 GO
 
 /*
@@ -104,13 +104,13 @@ GO
 DECLARE @hrule NCHAR(120) = REPLICATE('-', 120);
 PRINT @hrule;
 EXEC sp_configure
-	'Show Advanced Options',
-	1;
+    'Show Advanced Options',
+    1;
 RECONFIGURE;
 
 EXEC sp_configure
-	'OLE Automation Procedures',
-	1;
+    'OLE Automation Procedures',
+    1;
 RECONFIGURE;
 PRINT @hrule;
 
@@ -128,157 +128,157 @@ DECLARE @requestObjectType NVARCHAR(200) = 'MSXML2.ServerXMLHttp';
 DECLARE @obj INT;
 DECLARE @hresult INT;
 EXEC @hresult = sp_OACreate
-	@requestObjectType,
-	@obj OUT;
+    @requestObjectType,
+    @obj OUT;
 
 -- ensure the object actually got created
 IF @hresult != 0 BEGIN
-	DECLARE @errorMessage NVARCHAR(MAX) = CONCAT(
-		'Unable to create a new instance of ',
-		@requestObjectType,
-		' (error code ',
-		@hresult,
-		')'
-	);
+    DECLARE @errorMessage NVARCHAR(MAX) = CONCAT(
+        'Unable to create a new instance of ',
+        @requestObjectType,
+        ' (error code ',
+        @hresult,
+        ')'
+    );
 
-	EXEC sp_OADestroy
-		@obj;
+    EXEC sp_OADestroy
+        @obj;
 
-	THROW
-		50001,
-		@errorMessage,
-		1;
+    THROW
+        50001,
+        @errorMessage,
+        1;
 END;
 
 -- set up a proxy if the user provided one
 IF @httpProxy IS NOT NULL BEGIN
-	EXEC @hresult = sp_OAMethod
-		@obj,
-		'setProxy',
-		NULL,
-		'2',
-		@httpProxy;
+    EXEC @hresult = sp_OAMethod
+        @obj,
+        'setProxy',
+        NULL,
+        '2',
+        @httpProxy;
 
-	-- ensure that setting the proxy was successful
-	IF @hresult != 0 BEGIN
-		SET @errorMessage = CONCAT(
-			'Failed calling method ''setProxy'' of ',
-			@requestObjectType,
-			' (error code ',
-			@hresult,
-			')'
-		);
+    -- ensure that setting the proxy was successful
+    IF @hresult != 0 BEGIN
+        SET @errorMessage = CONCAT(
+            'Failed calling method ''setProxy'' of ',
+            @requestObjectType,
+            ' (error code ',
+            @hresult,
+            ')'
+        );
 
-		EXEC sp_OADestroy
-			@obj;
+        EXEC sp_OADestroy
+            @obj;
 
-		THROW
-			50003,
-			@errorMessage,
-			1;
-	END;
+        THROW
+            50003,
+            @errorMessage,
+            1;
+    END;
 END;
 
 -- initiate a new GET request to the Bootstrap URI and ensure the call was successful
 EXEC @hresult = sp_OAMethod
-	@obj,
-	'open',
-	NULL,
-	'GET',
-	@bootstrapUri,
-	false;
+    @obj,
+    'open',
+    NULL,
+    'GET',
+    @bootstrapUri,
+    false;
 
 IF @hresult != 0 BEGIN
-	SET @errorMessage = CONCAT(
-		'Failed calling method ''open'' of ',
-		@requestObjectType,
-		' (error code ',
-		@hresult,
-		')'
-	);
+    SET @errorMessage = CONCAT(
+        'Failed calling method ''open'' of ',
+        @requestObjectType,
+        ' (error code ',
+        @hresult,
+        ')'
+    );
 
-	EXEC sp_OADestroy
-		@obj;
+    EXEC sp_OADestroy
+        @obj;
 
-	THROW
-		50003,
-		@errorMessage,
-		1;
+    THROW
+        50003,
+        @errorMessage,
+        1;
 END;
 
 -- send the GET request and ensure that the call was successful
 EXEC @hresult = sp_OAMethod
-	@obj,
-	'send',
-	NULL,
-	'';
+    @obj,
+    'send',
+    NULL,
+    '';
 IF @hresult != 0 BEGIN
-	SET @errorMessage = CONCAT(
-		'Failed calling method ''send'' of ',
-		@requestObjectType,
-		' (error code ',
-		@hresult,
-		')'
-	);
+    SET @errorMessage = CONCAT(
+        'Failed calling method ''send'' of ',
+        @requestObjectType,
+        ' (error code ',
+        @hresult,
+        ')'
+    );
 
-	EXEC sp_OADestroy
-		@obj;
+    EXEC sp_OADestroy
+        @obj;
 
-	THROW
-		50004,
-		@errorMessage,
-		1;
+    THROW
+        50004,
+        @errorMessage,
+        1;
 END;
 
 -- get the results from the HTTP request and ensure that the call was successful
 DECLARE @results NVARCHAR(4000);
 EXEC @hresult = sp_OAMethod
-	@obj,
-	'responseText',
-	@results OUTPUT;
+    @obj,
+    'responseText',
+    @results OUTPUT;
 IF @hresult != 0 BEGIN
-	SET @errorMessage = CONCAT(
-		'Failed calling method ''responseText'' of ',
-		@requestObjectType,
-		' (error code ',
-		@hresult,
-		')'
-	);
+    SET @errorMessage = CONCAT(
+        'Failed calling method ''responseText'' of ',
+        @requestObjectType,
+        ' (error code ',
+        @hresult,
+        ')'
+    );
 
-	EXEC sp_OADestroy
-		@obj;
+    EXEC sp_OADestroy
+        @obj;
 
-	THROW
-		50005,
-		@errorMessage,
-		1;
+    THROW
+        50005,
+        @errorMessage,
+        1;
 END;
 PRINT @results;
 
 -- destroy the request object as we have the resultant Bootstrap procedure
 EXEC sp_OADestroy
-	@obj;
+    @obj;
 
 -- execute the results and create the Bootstrap procedure
 EXEC sp_executesql
-	@results;
+    @results;
 
 -- check if the Bootstrap procedure was actually created
 IF (
-	SELECT
-		COUNT(*)
-	FROM
-		INFORMATION_SCHEMA.ROUTINES
-	WHERE
-		[SPECIFIC_SCHEMA] = 'Pacify'
-		AND [SPECIFIC_NAME] = 'Bootstrap'
+    SELECT
+        COUNT(*)
+    FROM
+        INFORMATION_SCHEMA.ROUTINES
+    WHERE
+        [SPECIFIC_SCHEMA] = 'Pacify'
+        AND [SPECIFIC_NAME] = 'Bootstrap'
 ) = 0 BEGIN
-	SET @errorMessage = 'Pacify.Bootstrap procedure was not created';
+    SET @errorMessage = 'Pacify.Bootstrap procedure was not created';
 
-	THROW
-		50006,
-		@errorMessage,
-		1;
+    THROW
+        50006,
+        @errorMessage,
+        1;
 END;
 
 -- finally, execute the Bootstrap procedure
